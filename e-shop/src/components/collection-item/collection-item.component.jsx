951efe1a6db325ad6fbbc18ad138858e36471195
eddie chung <eddie.chung@moonfare.com>
2020-06-02 23:42:35 +0200

    finish sign in form

--- e-shop/src/components/collection-item/collection-item.component.jsx
+++ e-shop/src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import "./collection-item.styles.scss";
+import './collection-item.styles.scss';
 
-export const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className='collection-item'>
     <div
-      className="image"
+      className='image'
       style={{
-        backgroundImage: `url(${imageUrl})`,
+        backgroundImage: `url(${imageUrl})`
       }}
     />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
   </div>
 );
+
+export default CollectionItem;
