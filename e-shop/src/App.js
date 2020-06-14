88c91339f14bc770908691f5e37ec0a41dc5f979
eddie chung <eddie.chung@moonfare.com>
2020-06-14 22:31:58 +0200

    add cart item logic and convert connect to redux hooks

--- e-shop/src/App.js
+++ e-shop/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/header/header.component";
@@ -9,7 +9,10 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { setCurrentUser } from "./redux/user/userAction";
 
-function App({ setCurrentUser, currentUser }) {
+function App() {
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     let unsubscribeFromAuth = null;
 
@@ -17,17 +20,19 @@ function App({ setCurrentUser, currentUser }) {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapshot) => {
-          setCurrentUser({
-            id: snapshot.id,
-            ...snapshot.data(),
-          });
+          dispatch(
+            setCurrentUser({
+              id: snapshot.id,
+              ...snapshot.data(),
+            })
+          );
         });
       }
-      setCurrentUser(userAuth);
+      dispatch(setCurrentUser(userAuth));
     });
 
     return () => unsubscribeFromAuth();
-  }, [setCurrentUser]);
+  }, [dispatch]);
 
   return (
     <div>
@@ -47,12 +52,4 @@ function App({ setCurrentUser, currentUser }) {
   );
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- e-shop/src/components/collection-item/collection-item.component.jsx
+++ e-shop/src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
+import CustomButton from "../custom-button/custom-button.component";
+import "./collection-item.styles.scss";
+import { useDispatch } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
 
-import './collection-item.styles.scss';
+const CollectionItem = ({ item }) => {
+  const { imageUrl, name, price } = item;
+  const dispatch = useDispatch();
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => dispatch(addItem(item))}>
+        Add to cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
 export default CollectionItem;
