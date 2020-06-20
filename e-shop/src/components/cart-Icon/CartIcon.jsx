c5b1a5acfb807815178c03c4c1398847643ba810
Eddie Chung <designered@gmail.com>
2020-06-20 19:26:45 +0200

    add update card item quality logic

--- e-shop/src/components/cart-Icon/CartIcon.jsx
+++ e-shop/src/components/cart-Icon/CartIcon.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import "./cart-icon.styles.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import React from 'react';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import './cart-icon.styles.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 export const CartIcon = () => {
   const dispatch = useDispatch();
   const item = useSelector((state) => state.cart.cartItems);
 
   return (
-    <div className="cart-icon" onClick={() => dispatch(toggleCartHidden)}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{item.length}</span>
+    <div className='cart-icon' onClick={() => dispatch(toggleCartHidden())}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{item.length}</span>
     </div>
   );
 };
